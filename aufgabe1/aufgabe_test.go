83be59889deab98caeb86c4947c41961287473c7
Reiner Hüchting <dev@reinerhuechting.de>
2022-12-04 17:01:34 +0100

    Zwei weitere Tests hinzugefügt.

--- aufgabe1/aufgabe_test.go
+++ aufgabe1/aufgabe_test.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func ExampleCommonMultiples() {
 	fmt.Println(CommonMultiples(3, 5, 50))
+	fmt.Println(CommonMultiples(2, 10, 100))
+	fmt.Println(CommonMultiples(1, 1, 10))
 
 	// Output:
 	// [15 30 45]
+	// [10 20 30 40 50 60 70 80 90 100]
+	// [1 2 3 4 5 6 7 8 9 10]
 }
