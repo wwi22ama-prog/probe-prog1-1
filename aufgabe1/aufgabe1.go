57dfdd6fcb59c568275e3d4a31fd368ae769c6f5
Reiner Hüchting <dev@reinerhuechting.de>
2022-12-04 16:57:54 +0100

    Aufgabe hinzugefügt.

---
+++ aufgabe1/aufgabe1.go
@@ -0,0 +1,19 @@
+/* AUFGABE: Listen
+ * Erreichbare Punkte:
+ */
+package aufgabe1
+
+/* AUFGABENSTELLUNG */
+/*
+Erwartet drei Zahlen m,n,max > 0 und liefert eine Liste mit allen
+gemeinsamen Vielfachen von m und n, die nicht größer als max sind.
+*/
+func CommonMultiples(m, n, max int) []int {
+	result := []int{}
+	for x := m; x <= max; x += m {
+		if x%n == 0 {
+			result = append(result, x)
+		}
+	}
+	return result
+}
---
+++ aufgabe1/aufgabe_test.go
@@ -0,0 +1,10 @@
+package aufgabe1
+
+import "fmt"
+
+func ExampleCommonMultiples() {
+	fmt.Println(CommonMultiples(3, 5, 50))
+
+	// Output:
+	// [15 30 45]
+}
