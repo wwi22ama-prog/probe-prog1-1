c1abe8b32e2540097904fb4593d7c8f07ecbbf77
Reiner Hüchting <dev@reinerhuechting.de>
2022-12-05 22:39:03 +0100

    Lösung für Aufgabe 7 entfernt.

--- aufgabe7/aufgabe7.go
+++ aufgabe7/aufgabe7.go
@@ -11,14 +11,5 @@ package aufgabe7
 // Berechnet den ganzzahligen Quotienten x / y.
 // D.h. die Funktion ignoriert Nachkommastellen bzw. den Rest.
 func Div(x, y int) int {
-	if x < 0 {
-		return -Div(-x, y)
-	}
-	if y < 0 {
-		return -Div(x, -y)
-	}
-	if x < y {
-		return 0
-	}
 	return 1 + Div(x-y, y)
 }
